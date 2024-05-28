96ba4bee4f1bb8b39405a7ccaac30fe4e5aa9c76
Kyelink <143307261+Kyelink@users.noreply.github.com>
2024-05-28 15:16:05 +0200

    commit

--- exo4.dart
+++ exo4.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 bool isInt(String str) {
   try{
@@ -13,10 +14,20 @@ bool isInt(String str) {
 main() {
   print('Saisis un nombre :');
   String age = stdin.readLineSync();
-  while(!isInt(age)){
+  while(!isInt(age)||int.parse(age)<0){
     print('(format invalide) Saisis un nombre :');
     age = stdin.readLineSync();
   };
   int n = int.parse(age);
-  print((n%2==0)?"pair":"impair");
+  List<int> a = [];
+  for(int i=1;i<=sqrt(n);i++){
+    if(n%i==0){
+      a.add(i);
+      if(n/i!=i){a.add((n/i).round());}
+    }
+  }
+  a.sort();
+  print("diviseurs : ");
+  print(a);
+
 }
